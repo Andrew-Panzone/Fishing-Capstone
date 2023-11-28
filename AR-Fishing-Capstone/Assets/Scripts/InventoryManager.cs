75afbdf71818cf7b6ebc0ef5c95099132a9a0d4f
Danny Son <son.d@northeastern.edu>
2023-11-28 10:47:05 -0500

    test

--- AR-Fishing-Capstone/Assets/Scripts/InventoryManager.cs
+++ AR-Fishing-Capstone/Assets/Scripts/InventoryManager.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class InventoryManager : MonoBehaviour
 {
+
+    TextMeshProUGUI fishingPowerText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        updateFishingPowerText();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void onClickEquip()
+    {
+
+    }
+
+
+    void updateFishingPowerText()
+    {
+        fishingPowerText.text = "FP: " + Player.fishingPower;
     }
 }
